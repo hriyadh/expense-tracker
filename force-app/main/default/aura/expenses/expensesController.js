2b7a2efddc16ef8c8642c885f03a097329205b37
Riyadh Home <mail2riyadh@gmail.com>
2017-11-21 23:40:10 +0600

    expenseList and expenseItem component

--- force-app/main/default/aura/expenses/expensesController.js
+++ force-app/main/default/aura/expenses/expensesController.js
@@ -1,5 +1,15 @@
 ({
-    myAction : function(component, event, helper) {
+    clickCreate : function(component, event, helper) {
+        var validExpense = component.find('expenseform').reduce(function (validSofar, inputCmp) {
+            // Display error message for invalid fields
+            inputCmp.showHelpMessageIfInvalid();
+            return validSofar && inputCmp.get("v.validity").valid;
+        }, true);
 
+        if(validExpense) {
+            var newExpense = component.get('v.newExpense');
+            console.log(JSON.stringify(newExpense));
+            helper.createExpense(component, newExpense);
+        }
     }
 })
--- force-app/main/default/aura/expenses/expensesHelper.js
+++ force-app/main/default/aura/expenses/expensesHelper.js
@@ -1,5 +1,11 @@
 ({
-    helperMethod : function() {
-
+    createExpense : function(component, expense) {
+        var expenses = component.get("v.expenses");
+        var newExpense = JSON.parse(JSON.stringify(expense));
+        console.log(newExpense);
+        expenses.push(newExpense);
+        component.set("v.expenses", expenses);
+        console.log('-------------------------------');
+        console.log(JSON.stringify(expenses));
     }
 })
