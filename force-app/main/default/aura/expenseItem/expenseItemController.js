1ddd49319550d15a617c2ed326acd9b951302b62
Riyadh Hossain <mail2riyadh@gmail.com>
2018-04-11 22:04:02 +0600

    Add expense update event

--- force-app/main/default/aura/expenseItem/expenseItemController.js
+++ force-app/main/default/aura/expenseItem/expenseItemController.js
@@ -5,5 +5,12 @@
         if(mydate) {
             component.set("v.formatdate", new Date(mydate));
         }
+    },
+
+    clickReimbursed : function(component, event, helper) {
+        var expense = component.get("v.expense");
+        var updateEvent = component.getEvent("updateExpense");
+        updateEvent.setParams({ "expense" : expense });
+        updateEvent.fire();
     }
 })
