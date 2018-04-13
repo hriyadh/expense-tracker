a174e357266de6f7889945d0c06d41903d0887e6
Riyadh Hossain <mail2riyadh@gmail.com>
2018-04-13 23:17:52 +0600

    Reformat indentation

--- force-app/main/default/aura/expenseForm/expenseFormHelper.js
+++ force-app/main/default/aura/expenseForm/expenseFormHelper.js
@@ -1,6 +1,6 @@
 ({
 	createExpense : function(component, newExpense) {
-		var createEvent = component.getEvent("createExpense");	
+		var createEvent = component.getEvent("createExpense");
 		createEvent.setParams({ "expense" : newExpense});
 		createEvent.fire();
 	}
--- force-app/main/default/aura/expenses/expensesHelper.js
+++ force-app/main/default/aura/expenses/expensesHelper.js
@@ -1,30 +1,30 @@
 ({
-    saveExpense : function(component, expense, callback) {
-        var action = component.get("c.saveExpense");
-        action.setParams({
-            "expense" : expense
-        });
-        
-        if(callback) {
-            action.setCallback(this, callback);
-        }
+	saveExpense : function(component, expense, callback) {
+		var action = component.get("c.saveExpense");
+		action.setParams({
+			"expense" : expense
+		});
+		
+		if(callback) {
+			action.setCallback(this, callback);
+		}
 
-        $A.enqueueAction(action);
-    },
-    
-    createExpense : function(component, expense) {
+		$A.enqueueAction(action);
+	},
+	
+	createExpense : function(component, expense) {
 
-        this.saveExpense(component, expense, function(response) {
-            var state = response.getState();            
-            if(state === 'SUCCESS') {
-                var expenses = component.get("v.expenses");
-                expenses.push(response.getReturnValue());
-                component.set("v.expenses", expenses);
-            }
-        });
-    },
+		this.saveExpense(component, expense, function(response) {
+			var state = response.getState();            
+			if(state === 'SUCCESS') {
+				var expenses = component.get("v.expenses");
+				expenses.push(response.getReturnValue());
+				component.set("v.expenses", expenses);
+			}
+		});
+	},
 
-    updateExpense : function(component, expense) {
-        this.saveExpense(component, expense);
-    }
+	updateExpense : function(component, expense) {
+		this.saveExpense(component, expense);
+	}
 })
